86dc1cc5455e3df961c2bc6ccac028565386b401
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-10 22:31:43 +0800

    Create odd factor.c

---
+++ odd factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+	int d, m, n;
+	long s;
+	printf("ÇëÊäÈëÕûÊın£º");
+	scanf("%d", &n);
+	s = 0;
+	for (m = 1; m <= n; m++)
+	{
+		if (m % 2 > 0)
+		{
+			s = s + m;
+		}
+		else
+		{
+			d = m;
+			while (d % 2 == 0)
+			{
+				d = d / 2;
+			}
+			s = s - d;
+		}
+	}
+	printf("  s(%d)=%1d\n", n, s);
+	return 0;
+}
